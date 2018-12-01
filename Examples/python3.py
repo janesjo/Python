74289eb8d845d6b3ffc263af76e52adf282dfe1f
Jonathan Janesjo <jonathan.janesjo@outlook.com>
2018-12-01 23:18:44 +0100

    Added file read/write

--- Examples/python3.py
+++ Examples/python3.py
@@ -2,8 +2,22 @@
 
 # Open a file
 fo = open('foo.txt', 'w')
-fo.write( 'Name of the file: ' + fo.name)
-fo.write( 'Name of the file: ' + fo.name)
-fo.write( 'Name of the file: ' + fo.name)
-fo.write( 'Name of the file: ' + fo.name)
+
+# Write a int
+intVal = 1
+strIntVal = str(intVal)
+fo.writelines(strIntVal + "\n")
+
+# Write a string
+stringVal = "Hejsan"
+fo.writelines(stringVal + "\n")
+
+fo.close()
+
+# Open a file
+fo = open('foo.txt', 'r')
+
+intVal2 = int(fo.readline())
+stringVal2 = fo.readline().rstrip()
+
 fo.close()
